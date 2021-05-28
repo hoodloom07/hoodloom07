09ebc849d10e193ffa5d114073e0b63bfb5889c5
hoodloom07 <hoodloom07@gmail.com>
2021-05-28 02:55:37 -0500

    Archvio de inicializacion en fb

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firbase.initializeApp({
+    apiKey: "AIzaSyDW4JE6GjO_hXW-sgegtoYtp4UJMO7MN4M",
+    authDomain: "fbwebauth07.firebaseapp.com",
+    projectId: "fbwebauth07",
+    storageBucket: "fbwebauth07.appspot.com",
+    messagingSenderId: "423449394608",
+    appId: "1:423449394608:web:40e7c2fe0b78e7ddbfea75",
+    measurementId: "G-3RF04KN5FT"
+});
