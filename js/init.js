e8eba978e6acc1e824a03d0bda3d90b7d5f05e75
hoodloom07 <hoodloom07@gmail.com>
2021-05-28 04:07:12 -0500

    Cambios en archivo init de js

--- js/init.js
+++ js/init.js
@@ -1,4 +1,4 @@
-firbase.initializeApp({
+firebase.initializeApp({
     apiKey: "AIzaSyDW4JE6GjO_hXW-sgegtoYtp4UJMO7MN4M",
     authDomain: "fbwebauth07.firebaseapp.com",
     projectId: "fbwebauth07",
